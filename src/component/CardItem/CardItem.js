5a6f078ed9bc3173576d01db7f98e4639880264f
CarlosBorrero141 <cborrero094@gmail.com>
2021-06-13 10:13:10 -0500

    Contador

--- src/component/CardItem/CardItem.js
+++ src/component/CardItem/CardItem.js
@@ -2,34 +2,44 @@ import React from "react";
 import { Card, Icon, Image } from "semantic-ui-react";
 import "./CardItem.css";
 
-const CardItem = () => (
+function CardItem  () {
 
+  const [cantidad, setCantidad] = React.useState(0);
+
+  const sumar = () => {
+    setCantidad(cantidad + 1)
+  }
+    const restar = () => {
+      setCantidad(cantidad - 1);
+    };
   
 
-  <div className="cardList">
-    <Card>
-      <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-  </div>
-);
+ 
+  return (
+    <div className="cardList">
+      <Card>
+        <Image
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <button onClick={sumar}>+</button>
+          <p>{cantidad}</p>
+          <button onClick={restar}>-</button>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default CardItem;
