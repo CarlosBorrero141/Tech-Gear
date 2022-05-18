65b4819f0a62fbdbd4450e5ca7a3d029a098bdf9
Carlos Borrero <53616525+Sonrak141@users.noreply.github.com>
2022-05-18 08:29:07 -0500

    Update CartItems.js

--- src/component/CartItems/CartItems.js
+++ src/component/CartItems/CartItems.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState} from "react";
 import { CartContext } from "../../context/CartContext";
 import BuyerForm from "../BuyerForm/BuyerForm";
 
